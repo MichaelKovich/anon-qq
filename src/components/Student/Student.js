649a9e275f66d223b511ab25ffe2f7f50fd750f2
bkchu <brandon.k.chung@gmail.com>
2018-04-24 22:11:36 -0500

    added styling and changed the input to be on the student

--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
+import socketIOClient from "socket.io-client";
 
-const Student = () => {
-  return <div>This is the student component.</div>;
-};
+class Student extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: ""
+    };
+    this.socket = socketIOClient("http://localhost:3001/");
+  }
+
+  onSubmitHandler = e => {
+    e.preventDefault();
+    this.socket.emit("Message", this.state.message);
+    this.setState({ message: "" });
+  };
+
+  render() {
+    let { message } = this.state;
+    return (
+      <div className="Student">
+        <form onSubmit={this.onSubmitHandler}>
+          <input
+            className="Student__input"
+            onChange={e => this.setState({ message: e.target.value })}
+            value={message}
+            type="text"
+          />
+        </form>
+      </div>
+    );
+  }
+}
 
 export default Student;
--- src/components/Teacher/Teacher.js
+++ src/components/Teacher/Teacher.js
@@ -1,50 +1,46 @@
-import React, {Component} from 'react';
-import socketIOClient from 'socket.io-client';
+import React, { Component } from "react";
+import socketIOClient from "socket.io-client";
+
+import "./Teacher.css";
 
 class Teacher extends Component {
   constructor(props) {
     super(props);
     this.state = {
       code: null,
-      messages: [],
-      message: '',
+      messages: []
+      // message: ""
     };
-    this.socket = socketIOClient('http://192.168.1.154:3001/');
+    this.socket = socketIOClient("http://localhost:3001/");
   }
 
   componentDidMount() {
-    this.socket.on('Message', message => {
+    this.socket.on("Message", message => {
+      //id:
+      //text:
       const aggregatedMessages = [...this.state.messages, message];
-      this.setState({messages: aggregatedMessages});
-      console.log(this.state);
+      this.setState({ messages: aggregatedMessages });
     });
   }
 
-  onSubmitHandler = e => {
-    e.preventDefault();
-    this.socket.emit('Message', this.state.message);
-    this.setState({message: ''});
-  };
-
   render() {
-    let {code, messages, message} = this.state;
+    let { code, messages, message } = this.state;
     return (
       <div className="Teacher">
         The code is {code}.
-        <form onSubmit={this.onSubmitHandler}>
-          <input
-            onChange={e => this.setState({message: e.target.value})}
-            value={message}
-            type="text"
-          />
-        </form>
         <div className="Teacher__room">
           {messages[0] ? (
             messages.map(message => {
-              return <p key={Math.random()}>{message}</p>;
+              return (
+                <div className="Teacher__message" key={message.id}>
+                  {message.text}
+                </div>
+              );
             })
           ) : (
-            <p>Please enter a message.</p>
+            <p className="Teacher__message--none">
+              Waiting for student questions...
+            </p>
           )}
         </div>
       </div>
