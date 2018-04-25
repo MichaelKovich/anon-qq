a3cfd7ee136bc7ccfef4de1809185f7ec6e2c8a5
Michael Kovich <kovich@michaelkovich.com>
2018-04-25 14:42:48 -0500

    Rendering key on page.

--- src/components/Teacher/Teacher.js
+++ src/components/Teacher/Teacher.js
@@ -1,37 +1,48 @@
-import React, { Component } from "react";
-import socketIOClient from "socket.io-client";
+import React, {Component} from 'react';
+import socketIOClient from 'socket.io-client';
+import axios from 'axios';
 
-import "./Teacher.css";
+import './Teacher.css';
 
 class Teacher extends Component {
   constructor(props) {
     super(props);
     this.state = {
       code: null,
-      messages: []
+      messages: [],
     };
     this.socket = socketIOClient(process.env.REACT_APP_HOST);
   }
 
   componentDidMount() {
-    this.socket.on("get messages", messages => {
-      this.setState({ messages });
+    this.socket.on('get messages', (messages) => {
+      this.setState({messages});
     });
 
-    this.socket.on("delete message", messages => {
-      this.setState({ messages });
+    this.socket.on('delete message', (messages) => {
+      this.setState({messages});
     });
+
+    axios
+      .get('http://localhost:3001/keys/generate')
+      .then((response) => {
+        console.log(response);
+        this.setState({code: response.data.key});
+      })
+      .catch(err => console.log(err));
   }
 
   deleteHandler(id) {
-    this.socket.emit("delete message", id);
+    this.socket.emit('delete message', id);
   }
 
   render() {
-    const { code, messages } = this.state;
+    const {code, messages} = this.state;
     return (
       <div className="Teacher">
-        The code is {code}.
+        <div>
+          Your classroom code is: <p>{code}</p>
+        </div>
         <div className="Teacher__room">
           {messages && messages[0] ? (
             messages.map(message => (
@@ -44,9 +55,7 @@ class Teacher extends Component {
               </div>
             ))
           ) : (
-            <p className="Teacher__message--none">
-              Waiting for student questions...
-            </p>
+            <p className="Teacher__message--none">Waiting for student questions...</p>
           )}
         </div>
       </div>
